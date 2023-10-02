1429d24aa45c043bfb22477be8f5ef7a3bcf4281
Your Name <timofein90@gmail.com>
2023-10-03 01:31:12 +0500

    add books path to api

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "Hello? World!"
+
+@app.route('/books/<int:id>')
+def get_book(id):
+    book = {
+        "id": id,
+        "title": "Flask API Development Cookbook",
+        "author": "Stephen F. Lott",
+        "publication_date": "December 2019",
+        "publisher": "Packt Publishing",
+        "isbn": "9781789959384"
+    }
+    return jsonify(book)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
